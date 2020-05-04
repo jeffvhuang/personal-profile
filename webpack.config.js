eccc3eb92535031e684df9cf0812f9ee02861aea
jeff-h <jeffvh@outlook.com>
2020-05-04 16:19:09 +1000

    init react project

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var path = require('path');
+var HtmlWebpackPlugin =  require('html-webpack-plugin');
+
+module.exports = {
+    entry : './src/index.js',
+    output : {
+        path : path.resolve(__dirname , 'dist'),
+        filename: 'bundle.js'
+    },
+    module : {
+        rules : [
+            {
+                test : /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use:'babel-loader'
+            },
+            {
+                test : /\.css$/,
+                exclude: /node_modules/,
+                use:['style-loader', 'css-loader']
+            }
+        ]
+    },
+    mode:'development',
+    plugins : [
+        new HtmlWebpackPlugin ({
+            template : 'src/index.html'
+        })
+    ]
+}

7a5958fd08ca920d99c1431fc45b7b6bb7262356
jeff-h <jeffvh@outlook.com>
2020-05-05 18:13:10 +1000

    add tooltips to logos

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './header.css';
 import { FaEnvelope } from 'react-icons/fa';
+import ReactTooltip from "react-tooltip";
 
 const title = "Jeffrey Huang";
 const subtitle = "Software Developer";
@@ -22,15 +23,20 @@ class Header extends React.Component {
                         <h3>{subtitle}</h3>
                     </div>
                     <div className="page-header__logos">
-                        <a href={linkedin} target="_blank">
+                        <a href={linkedin} target="_blank" data-tip="LinkedIn">
                             <img id="linkedin-logo" className="logo" src="../../public/images/LI-In-Bug.png" alt="LinkedIn Logo" />
                         </a>
-                        <a href={github} target="_blank">
+                        <a href={github} target="_blank" data-tip="GitHub">
                             <img id="github-mark" className="logo" src="../../public/images/GitHub-Mark-64px.png" alt="GitHub Mark" />
                         </a>
-                        <div className="email logo" onClick={this.handleEmailClick}>
+                        <div
+                            className="email logo"
+                            onClick={this.handleEmailClick}
+                            data-tip={`Copy '${email}' to clipboard`}
+                        >
                             <FaEnvelope size="35rem" />
                         </div>
+                        <ReactTooltip place="top" type="dark" effect="float"/>
                     </div>
                 </div>
             </header>

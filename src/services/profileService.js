c24081d3f5fc25c466f9dc3e1440010f16f3f410
jeff-h <jeffvh@outlook.com>
2020-05-07 18:06:16 +1000

    separate data into separate file

--- src/services/profileService.js
+++ src/services/profileService.js
@@ -1,42 +1,11 @@
+const { details, projects } = require('./data')
+
 const ProfileService = {
     getPersonalDetails() {
-        const details = {
-            title: "Jeffrey Huang",
-            subtitle: "Software Developer",
-            linkedin: 'https://www.linkedin.com/in/jeffvhuang/',
-            github: 'https://github.com/jeffvhuang',
-            email: 'jeffvh@outlook.com'
-        }
         return new Promise((res) => res(details))
     },
 
     getProjects() {
-        const projects = [
-            {
-                title: 'Lets Chat',
-                imagePath: '../public/images/chat-bubbles.jpg',
-                link: 'https://jeffvh-lets-chat.herokuapp.com/',
-                github: 'https://github.com/jeffvhuang/lets-chat',
-                description: 'Real time chat application',
-                highlights: ['Node.js', 'Express', 'Socket.io']
-            },
-            {
-                title: 'Task Manager',
-                imagePath: '../public/images/project-management.jpg',
-                link: 'https://jeffvh-task-manager.herokuapp.com',
-                github: 'https://github.com/jeffvhuang/task-manager',
-                description: 'API for task management',
-                highlights: ['Node.js', 'Express', 'MongoDB']
-            },
-            {
-                title: 'Recaura',
-                imagePath: '../public/images/digital-hand.jpg',
-                link: '',
-                github: 'https://github.com/jeffvhuang/Recaura',
-                description: 'Physiotherapy management software',
-                highlights: ['React', 'Redux', '.NET Core', 'SQL', 'Docker']
-            }
-        ]
         return new Promise(res => res(projects))
     }
 }

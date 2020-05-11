d9ff7c3adf957433b9e444ce92e3d6419b54b961
jeff-h <jeffvh@outlook.com>
2020-05-11 22:34:37 +1000

    update Task Manager project info

--- src/services/data.js
+++ src/services/data.js
@@ -8,20 +8,20 @@ const details = {
 
 const projects = [
     {
+        title: 'Task Manager',
+        imagePath: '../../public/images/project-management.jpg',
+        link: 'https://jeffvh-task-manager.herokuapp.com',
+        github: 'https://github.com/jeffvhuang/task-manager',
+        description: 'API for task management',
+        highlights: ['React', 'Redux', 'Node.js', 'Express', 'MongoDB']
+    },
+    {
         title: 'PT Files',
         imagePath: '../../public/images/digital-hand.jpg',
         link: 'http://ptfiles-env.eba-rm3yrkqs.ap-southeast-2.elasticbeanstalk.com',
         github: 'https://github.com/jeffvhuang/pt-files',
         description: 'Physiotherapy management software',
         highlights: ['TypeScript', 'React', 'Redux', '.NET Core', 'SQL', 'AWS']
-    },
-    {
-        title: 'Task Manager',
-        imagePath: '../../public/images/project-management.jpg',
-        link: 'https://jeffvh-task-manager.herokuapp.com',
-        github: 'https://github.com/jeffvhuang/task-manager',
-        description: 'API for task management',
-        highlights: ['Node.js', 'Express', 'MongoDB']
     },
     {
         title: 'Lets Chat',
